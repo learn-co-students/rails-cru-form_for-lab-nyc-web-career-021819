9d34454ef5cc15ac6e2937f311a9f62a31dccbef
tchang9 <tlchang@gmail.com>
2019-03-17 02:24:19 -0400

    Done.

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,38 @@
+class ArtistsController < ApplicationController
+
+    def index
+        @artists = Artist.all
+        render :index
+    end
+
+    def new
+        @artist = Artist.new
+        render :new
+    end
+
+    def show
+        @artist = Artist.find_by(params[:id])
+        render :show
+    end
+
+    def create
+        @artist = Artist.create(artist_params)
+        redirect_to @artist
+    end
+
+    def edit
+        @artist = Artist.find_by(params[:id])
+        render :edit
+    end
+
+    def update
+        @artist = Artist.update(artist_params)
+        redirect_to @artist
+    end
+
+    private
+
+    def artist_params
+        params.require(:artist).permit(:name, :bio)
+    end
+end
